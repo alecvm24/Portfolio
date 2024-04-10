5ccfc0c9e039821fa7ec68886141df157e45f22b
Alec VM <alecvm24@hotmail.com>
2024-04-10 11:56:07 +0200

    Add logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,16 +13,16 @@ const Navbar = () => {
       className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
-        <link
-          to="/"
+        <a
+          href="/"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
             window.scrollTo(0, 0);
           }}
         >
-          {/*<img src={logo} alt="logo" className="w-9 h-9 object-contain" />*/}
-        </link>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+        </a>
       </div>
     </nav>
   );
