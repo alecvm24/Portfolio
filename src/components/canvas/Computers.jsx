e578d3c51df4c9428e52d4f78ee6ab68bd93511c
Alec VM <alecvm24@hotmail.com>
2024-04-12 15:20:13 +0200

    Add Computer canvas

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import { extend } from "@react-three/fiber";
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+// extend({ OrbitControls, Preload, useGLTF, TransformControls });
+import {
+  OrbitControls,
+  TransformControls,
+  Preload,
+  useGLTF,
+} from "@react-three/drei";
+
+import CanvasLoader from "../CanvasLoader";
+import { complex } from "framer-motion";
 
 const Computers = () => {
-  return (
-    <div>Computers</div>
-  )
-}
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
-export default Computers
+  return (
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor="black" />
+      <pointLight intensity={1} />
+      <primitive object={computer.scene} />
+    </mesh>
+  );
+};
+
+const ComputersCanvas = () => {
+  return (
+    <>
+      <Canvas
+        frameloop="demand"
+        shadows
+        camera={{ position: [20, 3, 5], fov: 25 }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls
+            enableZoom={false}
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2}
+          />
+          <Computers />
+        </Suspense>
+
+        <Preload all />
+      </Canvas>
+    </>
+  );
+};
+
+export default ComputersCanvas;
