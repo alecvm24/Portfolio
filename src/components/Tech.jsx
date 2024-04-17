8ed7df55b0eb2a77b46b825f01fbeaff0f6da0cd
Alec VM <alecvm24@hotmail.com>
2024-04-17 16:38:44 +0200

    Add Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+import { technologies } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { BallCanvas } from "./canvas";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
