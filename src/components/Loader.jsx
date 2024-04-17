b14a2f116fa6e29fbeee1f01be84022507498627
Alec VM <alecvm24@hotmail.com>
2024-04-17 09:58:37 +0200

    Add loader for the Canvas

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import { Html, useProgress } from "@react-three/drei";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
 
-const CanvasLoader = () => {
-  return <>Loader</>;
+const Loader = () => {
+  const { progress } = useProgress();
+
+  return (
+    <Html>
+      <span className="canvas-load"></span>
+      <p
+        style={{
+          fontSize: 14,
+          color: "#f1f1f1",
+          fontWeight: 800,
+          marginTop: 40,
+        }}
+      >
+        {progress.toFixed(2)}%
+      </p>
+    </Html>
+  );
 };
 
-export default CanvasLoader;
+export default Loader;
