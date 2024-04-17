3a9bf7a1572a4a33099d61a242244be1f3038554
Alec VM <alecvm24@hotmail.com>
2024-04-17 11:26:20 +0200

    Add dummycontent to about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
+import { Tilt } from "react-tilt";
+import { styles } from "../styles";
+
+import { services } from "../constants";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => {
+  return <p>{title}</p>;
+};
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
 
-export default About
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam voluptates
+        placeat in tempora maiores iure consectetur, eius, eveniet iusto
+        reiciendis molestias! Facere minus ratione nesciunt delectus asperiores
+        iure, cupiditate recusandae! Lorem ipsum dolor sit amet, consectetur
+        adipisicing elit. Nam voluptates placeat in tempora maiores iure
+        consectetur, eius, eveniet iusto reiciendis molestias! Facere minus
+        ratione nesciunt delectus asperiores iure, cupiditate recusandae!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default About;
