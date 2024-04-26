8a7246e3c478fbabcd571efdde82fbe6a6950714
Alec VM <alecvm24@hotmail.com>
2024-04-26 18:49:45 +0200

    Make testimonial card

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -9,7 +9,17 @@ const Feedbacks = () => {
     <div className="mt-12 bg-black-100 rounded-[20px]">
       <div
         className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}
-      ></div>
+      >
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>what others say</p>
+          <h2 className={styles.sectionHeadText}>Testimonials.</h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pb-14 flex flex-wrap gap-7`}>
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard key={testimonial.name} index={index} {...testimonial} />
+        ))}
+      </div>
     </div>
   );
 };
