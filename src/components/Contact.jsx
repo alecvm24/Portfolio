6c72e9fd207b2e127c5b107cd5c7a777682b875c
Alec VM <alecvm24@hotmail.com>
2024-05-03 20:09:11 +0200

    Add Title + bg

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 import { motion } from "framer-motion";
-import { emailjs } from "@emailjs/browser";
+import emailjs from "@emailjs/browser";
 
 import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
@@ -18,6 +18,18 @@ const Contact = () => {
 
   const handleChange = (e) => {};
   const handleSubmit = (e) => {};
+
+  return (
+    <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in toouch</p>
+        <h3 className={styles.sectionHeadText}>Contact.</h3>
+      </motion.div>
+    </div>
+  );
 };
 
-export default Contact;
+export default SectionWrapper(Contact, "contact");
