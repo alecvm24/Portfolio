a2a77a1aff0b1239c0503b770132a07e4b32b966
Alec VM <alecvm24@hotmail.com>
2024-05-03 18:36:35 +0200

    Fix bug

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -10,9 +10,9 @@ const SectionWrapper = (Component, idName) =>
         initial="hidden"
         whileInView="show"
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-x-7xl mx-auto relative z-0`}
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className="hash-hash" id={idName}>
+        <span className="hash-span" id={idName}>
           &nbsp;
         </span>
         <Component />
