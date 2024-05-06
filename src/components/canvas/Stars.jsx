00e1d0bbdf516bde89997bc47274665b1fe10323
Alec VM <alecvm24@hotmail.com>
2024-05-06 18:50:23 +0200

    Make Stars appear and change import bug

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,23 +1,39 @@
 import { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
-import * as random from "maath/random/dist";
+import * as random from "maath/random/dist/maath-random.esm";
 
 const Stars = (props) => {
-  return <div>Stars</div>;
+  const ref = useRef();
+
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#f272c8"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
-const starsCanvas = () => {
+const StarsCanvas = () => {
   return (
-    <div className="w-full h-full absolute inset-0 z-[-1]">
-      <canvas camera={{ position: [0, 0, 1] }}>
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
           <Stars />
         </Suspense>
+
         <Preload all />
-      </canvas>
+      </Canvas>
     </div>
   );
 };
 
-export default starsCanvas;
+export default StarsCanvas;
