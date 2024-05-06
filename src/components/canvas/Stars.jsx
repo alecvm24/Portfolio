9a9906448c7e75f70c037a22088620b1c3b087f8
Alec VM <alecvm24@hotmail.com>
2024-05-06 17:59:28 +0200

    Add starCanvas

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import { useState, useRef, Suspense } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist";
 
-const Stars = () => {
+const Stars = (props) => {
+  return <div>Stars</div>;
+};
+
+const starsCanvas = () => {
   return (
-    <div>Stars</div>
-  )
-}
+    <div className="w-full h-full absolute inset-0 z-[-1]">
+      <canvas camera={{ position: [0, 0, 1] }}>
+        <Suspense fallback={null}>
+          <Stars />
+        </Suspense>
+        <Preload all />
+      </canvas>
+    </div>
+  );
+};
 
-export default Stars
+export default starsCanvas;
